d483ce9c446bfe160c12bd4e0234c9d49aa16de6
ASEEL SHAHEEN <aseelshaheen1@gmail.com>
2024-02-21 01:09:09 +0200

    test

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleChatServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleChatServer.java
@@ -11,7 +11,8 @@ public class SimpleChatServer {
     private static SimpleServer server;
 
     public static void main(String[] args) throws IOException {
-        server = new SimpleServer(3000);
+        server = new SimpleServer(3000); // create server and initialize it(crates the database and tables, and
+                                         // populates them with data)
         System.out.println("Server is running");
         server.listen();
     }
